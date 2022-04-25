0d1a8a8e5fe250415fcab52bc59f7651f3c13475
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-04-25 16:47:26 +0100

    postiing articles

--- routes/blog.js
+++ routes/blog.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Article = require('../models/article')
 const router = express.Router();
 
 router.get('/',(req,res)=>{
@@ -10,7 +11,25 @@ router.get('/',(req,res)=>{
     res.render('index',{articles:articles});
 });
 
-router.get('/test/',(req,res)=>{
-    res.render('test')});
+router.get('/new',(req,res)=>{
+    res.render('/blog/new')});
+router.get('/:id',(req,res)=>{
+
+})
+
+router.post('/',async (req,res)=>{
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try{
+        article = await article.save();
+        res.redirect(`/blog/${article.id}`)
+    }catch(err){
+        res.render('/blog/new',{article: article})
+    }
+
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const blogRouter = require('./routes/blog')
+
+//mongoose.connect('mongodb://127.0.0.1/blog',{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://laajili:test1234@nodetuto.n1hyy.mongodb.net/BlogProject?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology:true});
 app.listen(5000);
 
 app.set('view engine','ejs');
 
 app.use('/blog',blogRouter);
+app.use(express.urlencoded({extended:true}));
